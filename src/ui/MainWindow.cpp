e99fe2c472c3bd405338e656da45379409df9c1a
Atto Allas <atto.allas@gmail.com>
2020-06-20 11:57:23 +0100

    Basically nothing

---
+++ src/ui/MainWindow.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Attoa on 20/06/2020.
+//
+
+#include "MainWindow.h"
---
+++ src/ui/MainWindow.h
@@ -0,0 +1,14 @@
+//
+// Created by Attoa on 20/06/2020.
+//
+
+#ifndef BEACON_CORE_MAINWINDOW_H
+#define BEACON_CORE_MAINWINDOW_H
+
+
+class MainWindow {
+
+};
+
+
+#endif //BEACON_CORE_MAINWINDOW_H
