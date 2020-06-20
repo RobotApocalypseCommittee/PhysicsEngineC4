09908d9ad8e76485bcf114da0f97c764b9a5d075
Joe <joe.bell34@btinternet.com>
2020-06-20 12:32:25 +0100

    Okay

--- src/ui/MainWindow.cpp
+++ src/ui/MainWindow.cpp
@@ -2,4 +2,20 @@
 // Created by Attoa on 20/06/2020.
 //
 
-#include "MainWindow.h"
+#include <QApplication>
+#include <QMainWindow>
+
+int main(int argc, char** argv) {
+#ifdef Q_OS_ANDROID
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+    QApplication app(argc, argv);
+    QCoreApplication::setOrganizationName("QtProject");
+    QCoreApplication::setApplicationName("Application Example");
+    QCoreApplication::setApplicationVersion(QT_VERSION_STR);
+
+    QMainWindow mainWin;
+    mainWin.show();
+    return QApplication::exec();
+}
